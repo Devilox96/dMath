bc7cef07cff75141c8a4e86f732d2088d17f8c80
Andrey Sadkov <devilox96@yandex.ru>
2020-03-05 20:16:15 +0300

    Added some Runge-Kutta schemes

--- NumerCalc/dRungeKutta.h
+++ NumerCalc/dRungeKutta.h
@@ -1,23 +1,90 @@
 #ifndef DRUNGEKUTTA_H
 #define DRUNGEKUTTA_H
 //-----------------------------//
-#include <iostream>
-#include <vector>
+#include <stdexcept>
 //-----------------------------//
-#include "../Core/dVector.h" //---dMath/Core/dVector.h---//
-//-----------------------------//
-struct dRungeKuttaParam {
-    double Step = 0.1;
+template <class T>
+class dRungeKutta1D {
+public:
+    dRungeKutta1D() : mStepTime(0.0) {}
+    explicit dRungeKutta1D(double tStepTime) : mStepTime(tStepTime) {}
 
-    double Arg = 0.0;
-    dVector2D <double> ZeroDerInit;
-    dVector2D <double> FirstDerInit;
+    T solve2nd(double tTime, const T& tInit, T (*tFunc)(double tTime, const T& tArg)) {
+        if (!mStepTime) {
+            throw std::runtime_error("dRungeKutta1D::solve2nd: time is not provided!");
+        }
 
-    virtual dVector2D <double> Func(double ArgP, dVector2D <double> ZeroDerInitP) = 0;
-    virtual dVector2D <double> Func(double ArgP, dVector2D <double> ZeroDerInitP, dVector2D <double> FirstDerInitP) = 0;
+        T U1 = tInit + mStepTime / 2.0 * tFunc(tTime, tInit);
+        T U2 = tInit + mStepTime * tFunc(tTime + mStepTime / 2.0, U1);
+
+        return U2;
+    }
+    T solve2nd(const T& tInit, T (*tFunc)(const T& tArg)) {
+        T U1 = tInit + mStepTime / 2.0 * tFunc(tInit);
+        T U2 = tInit + mStepTime * tFunc(U1);
+
+        return U2;
+    }
+    T solve4th(double tTime, const T& tInit, T (*tFunc)(double tTime, const T& tArg)) {
+        if (!mStepTime) {
+            throw std::runtime_error("dRungeKutta1D::solve4th: time is not provided!");
+        }
+
+        double HalfStep = mStepTime / 2.0;
+
+        T U1 = tInit + HalfStep * tFunc(tTime, tInit);
+        T U2 = tInit + HalfStep * tFunc(tTime + HalfStep, U1);
+        T U3 = tInit + mStepTime * tFunc(tTime + HalfStep, U2);
+        T U4 = (-tInit + U1 + 2.0 * U2 + U3 + HalfStep * tFunc(tTime + mStepTime, U3)) / 3.0;
+
+        return U4;
+    }
+    T solve4th(const T& tInit, T (*tFunc)(const T& tArg)) {
+        double HalfStep = mStepTime / 2.0;
+
+        T U1 = tInit + HalfStep * tFunc(tInit);
+        T U2 = tInit + HalfStep * tFunc(U1);
+        T U3 = tInit + mStepTime * tFunc(U2);
+        T U4 = (-tInit + U1 + 2.0 * U2 + U3 + HalfStep * tFunc(U3)) / 3.0;
+
+        return U4;
+    }
+    T solve2ndTVD(double tTime, const T& tInit, T (*tFunc)(double tTime, const T& tArg)) {
+        if (!mStepTime) {
+            throw std::runtime_error("dRungeKutta1D::solve2ndTVD: time is not provided!");
+        }
+
+        T U1 = tInit + mStepTime * tFunc(tTime, tInit);
+        T U2 = 0.5 * (tInit + U1 + mStepTime * tFunc(tTime + mStepTime, U1));
+
+        return U2;
+    }
+    T solve2ndTVD(const T& tInit, T (*tFunc)(const T& tArg)) {
+        T U1 = tInit + mStepTime * tFunc(tInit);
+        T U2 = 0.5 * (tInit + U1 + mStepTime * tFunc(U1));
+
+        return U2;
+    }
+    T solve3rdTVD(double tTime, const T& tInit, T (*tFunc)(double tTime, const T& tArg)) {
+        if (!mStepTime) {
+            throw std::runtime_error("dRungeKutta1D::solve3rdTVD: time is not provided!");
+        }
+
+        T U1 = tInit + mStepTime * tFunc(tTime, tInit);
+        T U2 = tInit + 0.25 * (3.0 * tInit + U1 + mStepTime * tFunc(tTime + mStepTime, U1));
+        T U3 = (tInit + 2.0 * U2 + 2.0 * mStepTime * tFunc(tTime + mStepTime / 2.0, U2)) / 3.0;
+
+        return U3;
+    }
+    T solve3rdTVD(const T& tInit, T (*tFunc)(const T& tArg)) {
+        T U1 = tInit + mStepTime * tFunc(tInit);
+        T U2 = 0.25 * (3.0 * tInit + U1 + mStepTime * tFunc(U1));
+        T U3 = (tInit + 2.0 * U2 + 2.0 * mStepTime * tFunc(U2)) / 3.0;
+
+        return U3;
+    }
+private:
+    double mStepTime;
 };
-//-----------------------------//
-void dRungeKutta4th_1(dRungeKuttaParam& ParamP);
-void dRungeKutta4th_2(dRungeKuttaParam& ParamP);
 //-----------------------------//
 #endif
--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #include <chrono>
 #include <QVector3D>
 
+#include "NumerCalc/dRungeKutta.h"
+
 template<typename F, typename... Args>
 void test(F func, Args&&... args){
     std::clock_t start = std::clock();
@@ -19,53 +21,61 @@ void test(F func, Args&&... args){
     std::cout <<  double(end - start) / CLOCKS_PER_SEC << " s\n";
 }
 
+double Func(double tTime, const double& tArg) {
+    return (tArg + tTime) * sin(tTime);
+}
+
+double Func2(const double& tArg) {
+    return tArg * sin(tArg);
+}
 
 int main() {
-//    dVectorND <double, 5> Temp1(0.1, 0.2, 0.3, 0.4, 0.5);
-//    Vector <double, 5> Temp2({0.1, 0.2, 0.3, 0.4, 0.5});
-//    dVectorND <double, 5> Add1(0.1, 0.1, 0.1, 0.1, 0.1);
-//    Vector <double, 5> Add2({0.1, 0.1, 0.1, 0.1, 0.1});
-//
-////    std::cout << "Result:" << Temp1 << std::endl;
-//
-////    std::cout << Temp1 * Add1 << std::endl;
-//
     auto Start = std::chrono::system_clock::now();
 
-    for (long int i = 0; i < 100'000'000; i++) {
-        (QVector3D (0.1, 0.2, 0.3) + QVector3D (0.1, 0.2, 0.3)) / i;
+    //----------//
+
+    double Arg = 1;
+    double TimeArg = 0;
+    double TimeStep = 1.0;
+
+    dRungeKutta1D <double> Solver(1.0);
+
+    std::cout << Arg << std::endl;
+
+    for (int i = 0; i < 10; i++) {
+        Arg = Solver.solve4th(TimeArg, Arg, Func);
+//        Arg = Solver.solve4th(Arg, Func2);
+        TimeArg += TimeStep;
+        std::cout << TimeArg << ": " << Arg << std::endl;
     }
+
+    //----------//
 
     auto Stop = std::chrono::system_clock::now();
 
     std::chrono::duration <double> Time = Stop - Start;
-    std::cout << "QVector3D: " << Time.count() << "s" << std::endl;
-
-    //----------//
-
-    Start = std::chrono::system_clock::now();
-
-    for (long int i = 0; i < 100000000; i++) {
-        (dVector <float, 3>(0.1, 0.2, 0.3) + dVector <float, 3>(0.1, 0.2, 0.3)) / i;
-    }
-
-    Stop = std::chrono::system_clock::now();
-
-    Time = Stop - Start;
-    std::cout << "dVector: " << Time.count() << "s" << std::endl;
-
-    //----------//
+//    std::cout << "Function: " << Time.count() << "s" << std::endl;
+//
+//    //----------//
+//
+//    Arg = 1;
+//    TimeArg = 0;
+//    TimeStep = 1.0;
 //
 //    Start = std::chrono::system_clock::now();
 //
-//    for (int i = 0; i < 150000000; i++) {
-//        Temp2 == Add2;
-//    }
+//    //----------//
+//
+//
+//
+//    //----------//
 //
 //    Stop = std::chrono::system_clock::now();
 //
 //    Time = Stop - Start;
-//    std::cout << "dVectorND comparison: " << Time.count() << "s" << std::endl;
+//    std::cout << "Pointer: " << Time.count() << "s" << std::endl;
+
+    //----------//
 
     return EXIT_SUCCESS;
 }
