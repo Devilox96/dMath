b8504fb44ee3f6edcb821e0a29a15aa6f2bfbdff
Andrey Sadkov <devilox96@yandex.ru>
2020-03-26 18:52:51 +0300

    Gonna test on 1D shallow water problem

--- main.cpp
+++ main.cpp
@@ -51,16 +51,40 @@ double Func(double tVal) {
     return tVal * tVal / 2.0;
 }
 
+double WENO_2(const double& tVal_minus, const double& tVal, const double& tVal_plus, bool tNegFlux) {
+    double V0 = 0.5 * tVal + 0.5 * tVal_plus;       //---r = 0---//
+    double V1 = -0.5 * tVal_minus + 1.5 * tVal;     //---r = 1---//
+
+    double Beta0 = pow(tVal_plus - tVal, 2.0);
+    double Beta1 = pow(tVal - tVal_minus, 2.0);
+
+    double Alpha0;
+    double Alpha1;
+
+    if (tNegFlux) {
+        Alpha0 = 1.0 / 3.0 / pow(1.0e-06 + Beta0, 2.0);
+        Alpha1 = 2.0 / 3.0 / pow(1.0e-06 + Beta1, 2.0);
+    } else {
+        Alpha0 = 2.0 / 3.0 / pow(1.0e-06 + Beta0, 2.0);
+        Alpha1 = 1.0 / 3.0 / pow(1.0e-06 + Beta1, 2.0);
+    }
+
+    double Omega0 = Alpha0 / (Alpha0 + Alpha1);
+    double Omega1 = Alpha1 / (Alpha0 + Alpha1);
+
+    return Omega0 * V0 + Omega1 * V1;
+}
+
 int main() {
     size_t Num = 200;
 
     double ul = 30;
     double ur = 10;
     double alpha = 1.0;
-    double xl = 15.0;
-//    double xl = 0.0;
-    double xr = 35.0;
-//    double xr = 20.0;
+//    double xl = 15.0;
+    double xl = 0.0;
+//    double xr = 35.0;
+    double xr = 20.0;
 
     int Sec = 4;
 
@@ -89,113 +113,45 @@ int main() {
         GridSecond[i] = ur;
     }
 
-    std::ofstream Output;
-    std::ofstream ROutput;
     std::ofstream EOutput;
     std::ofstream Exact;
 
-    Output.open("Amplitude.dat");
-    ROutput.open("RAmplitude.dat");
     EOutput.open("EAmplitude.dat");
     Exact.open("Exact.dat");
 
-    //-----------------------------//
+    for (int i = 0; i < Sec * 1000; i++) {
+        double Alpha = 0.0;
 
-    LaxSolver Solver;
-
-    Solver.setTimeStep(0.01);
-    Solver.setXStep(1.0);
-
-    for (int i = 0; i < Sec * 100; i++) {
-        for (size_t j = 1; j < Num - 1; j++) {
-            (*Second)[j] = Solver.solve((*First)[j - 1], (*First)[j + 1]);
+        for (const auto& iNum : (*First)) {
+            Alpha = std::max(iNum, Alpha);
         }
 
-        (*Second)[0] = ul;
-        (*Second)[Num - 1] = ur;
+        for (size_t j = 2; j < Num - 1; j++) {
+            double PlusPlus = WENO_2(
+                    (Func((*First)[j - 1]) + Alpha * (*First)[j - 1]) / 2.0,
+                    (Func((*First)[j]) + Alpha * (*First)[j]) / 2.0,
+                    (Func((*First)[j + 1]) + Alpha * (*First)[j + 1]) / 2.0, false);
+            double PlusMinus = WENO_2(
+                    (Func((*First)[j - 1]) - Alpha * (*First)[j - 1]) / 2.0,
+                    (Func((*First)[j]) - Alpha * (*First)[j]) / 2.0,
+                    (Func((*First)[j + 1]) - Alpha * (*First)[j + 1]) / 2.0, true);
 
-        std::swap(First, Second);
-    }
+            double MinusPlus = WENO_2(
+                    (Func((*First)[j - 2]) + Alpha * (*First)[j - 2]) / 2.0,
+                    (Func((*First)[j - 1]) + Alpha * (*First)[j - 1]) / 2.0,
+                    (Func((*First)[j]) + Alpha * (*First)[j]) / 2.0, false);
+            double MinusMinus = WENO_2(
+                    (Func((*First)[j - 2]) - Alpha * (*First)[j - 2]) / 2.0,
+                    (Func((*First)[j - 1]) - Alpha * (*First)[j - 1]) / 2.0,
+                    (Func((*First)[j]) - Alpha * (*First)[j]) / 2.0, true);
 
-    for (size_t i = 0; i < Num; i++) {
-        Output << (*First)[i] << " ";
-    }
-
-    //---Lax---//
-    //---Richtmyer---//
-
-    for (size_t i = 0; i < xl; i++) {
-        GridFirst[i] = ul;
-        GridSecond[i] = ul;
-    }
-
-    for (size_t i = xl; i < xr; i++) {
-        GridFirst[i] = ul - alpha * double(i - xl);
-        GridSecond[i] = ul - alpha * double(i - xl);
-    }
-
-    for (size_t i = xr; i < Num; i++) {
-        GridFirst[i] = ur;
-        GridSecond[i] = ur;
-    }
-
-    RichtmyerSolver RSolver;
-
-    RSolver.setTimeStep(0.02);
-    RSolver.setXStep(1.0);
-
-    for (int i = 0; i < Sec * 50; i++) {
-        for (size_t j = 1; j < Num - 1; j++) {
-            (*Second)[j] = RSolver.solve((*First)[j - 1], (*First)[j], (*First)[j + 1]);
-        }
-
-        (*Second)[0] = ul;
-        (*Second)[Num - 1] = ur;
-
-        std::swap(First, Second);
-    }
-
-    for (size_t i = 0; i < Num; i++) {
-        ROutput << (*First)[i] << " ";
-    }
-
-    //---ENO---//
-
-    for (size_t i = 0; i < xl; i++) {
-        GridFirst[i] = ul;
-        GridSecond[i] = ul;
-    }
-
-    for (size_t i = xl; i < xr; i++) {
-        GridFirst[i] = ul - alpha * double(i - xl);
-        GridSecond[i] = ul - alpha * double(i - xl);
-    }
-
-    for (size_t i = xr; i < Num; i++) {
-        GridFirst[i] = ur;
-        GridSecond[i] = ur;
-    }
-
-    for (int i = 0; i < 200; i++) {
-        for (size_t j = 2; j < Num - 2; j++) {
-            double Plus =
-                    (-1.0 / (*First)[j - 1] * (*First)[j - 1] +
-                    7.0 / (*First)[j] * (*First)[j] +
-                    7.0 / (*First)[j + 1] * (*First)[j + 1] -
-                    1.0 / (*First)[j + 2] * (*First)[j + 2]) / 24.0;
-            double Minus =
-                    (-1.0 / (*First)[j - 2] * (*First)[j - 2] +
-                     7.0 / (*First)[j - 1] * (*First)[j - 1] +
-                     7.0 / (*First)[j] * (*First)[j] -
-                     1.0 / (*First)[j + 1] * (*First)[j + 1]) / 24.0;
-
-            (*Second)[j] = (*First)[j] - 0.02 / 1.0 * (Plus - Minus);
+            (*Second)[j] = (*First)[j] - 0.001 / 1.0 * (PlusPlus + PlusMinus - MinusPlus - MinusMinus);
         }
 
         (*Second)[0] = ul;
         (*Second)[1] = ul;
         (*Second)[Num - 1] = ur;
-        (*Second)[Num - 2] = ur;
+//        (*Second)[Num - 2] = ur;
 //        (*Second)[Num - 3] = ur;
 
         std::swap(First, Second);
@@ -218,7 +174,7 @@ int main() {
             GridExact[i] = ur;
         }
 
-        std::cout << i << std::endl;
+//        std::cout << i << std::endl;
     }
 
     for (size_t i = 0; i < Num; i++) {
