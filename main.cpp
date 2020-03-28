dc5cbc2762b1eae896b6e7ab6abcba306e550917
Andrey Sadkov <devilox96@yandex.ru>
2020-03-28 20:27:54 +0300

    Looks like the 2D solver works

--- main.cpp
+++ main.cpp
@@ -8,107 +8,58 @@
 #include "Core/dMatrix.h"
 
 #include "NumerCalc/dRungeKutta.h"
-#include "NumerCalc/dENO.h"
-#include "NumerCalc/dLaxFriedrichs1D.h"
-#include "NumerCalc/dRichtmyer1D.h"
-
-template<typename F, typename... Args>
-void test(F func, Args&&... args) {
-    std::clock_t start = std::clock();
-    for (int i = 0; i < 150'000'000; ++i) {
-        func(std::forward<Args>(args)...);
-    }
-    std::clock_t end = std::clock();
-
-    std::cout << double(end - start) / CLOCKS_PER_SEC << " s\n";
-}
-
-class LaxSolver : public dLaxFriedrichs1D <double> {
-private:
-    double func(const double& tVal) override {
-        return tVal * tVal / 2.0;
-    }
-};
-
-class RichtmyerSolver : public dRichtmyer1D <double> {
-private:
-    double func(const double& tVal) override {
-        return tVal * tVal / 2.0;
-    }
-};
-
-double getEnergy(const std::vector <double>& tGrid) {
-    double Energy = 0.0;
-
-    for (const auto& iElem : tGrid) {
-        Energy += iElem;
-    }
-
-    return Energy;
-}
 
 //---Problem---//
 
-dVector2D <double> funcX(const dVector2D <double>& tVal) {
-    return dVector2D <double>(
+dVector3D <double> funcX(const dVector3D <double>& tVal) {
+    return dVector3D <double>(
             tVal[1],
-            tVal[1] * tVal[1] / tVal[0] + 0.5 * 9.81 * tVal[0] * tVal[0]
-            );
+            tVal[1] * tVal[1] / tVal[0] + 0.5 * 9.81 * tVal[0] * tVal[0],
+            tVal[1] * tVal[2] / tVal[0]
+    );
+}
+dVector3D <double> funcY(const dVector3D <double>& tVal) {
+    return dVector3D <double>(
+            tVal[2],
+            tVal[1] * tVal[2] / tVal[0],
+            tVal[2] * tVal[2] / tVal[0] + 0.5 * 9.81 * tVal[0] * tVal[0]
+    );
 }
 
-void bottomFunc(std::vector <double>& tVec) {
-    for (int i = 0; i < 200; i++) {
-        tVec[i] = 25.0;
-    }
+void bottomFunc(std::vector <std::vector <double>>& tVec) {
+    for (int i = 0; i < 604; i++) {
+        for (int j = 0; j < 202; j++) {
+            tVec[i][j] = 25.0;
+        }
 
-    for (int i = 200; i < 400; i++) {
-        tVec[i] = 25.0 - static_cast <double>(i - 200) / 8;
-    }
+        for (int j = 202; j < 402; j++) {
+            tVec[i][j] = 25.0 - static_cast <double>(j - 202) / 8;
+        }
 
-    for (int i = 400; i < 600; i++) {
-        tVec[i] = 0.0;
+        for (int j = 402; j < 604; j++) {
+            tVec[i][j] = 0.0;
+        }
     }
 }
+void initConditions(std::vector <std::vector <dVector3D <double>>>& tGrid) {
+    for (int i = 0; i < 604; i++) {
+        for (int j = 0; j < 202; j++) {
+            tGrid[i][j][0] = 100.0;
+            tGrid[i][j][1] = 0.0;
+        }
 
-void initConditions(std::vector <dVector2D <double>>& tGrid) {
-    for (int i = 0; i < 200; i++) {
-        tGrid[i][0] = 100.0;
-        tGrid[i][1] = 0.0;
-    }
-
-    for (int i = 200; i < 600; i++) {
-        tGrid[i][0] = 50.0;
-        tGrid[i][1] = 0.0;
+        for (int j = 202; j < 604; j++) {
+            tGrid[i][j][0] = 50.0;
+            tGrid[i][j][1] = 0.0;
+        }
     }
 }
 
 //---Problem---//
 
-//double WENO_2(const double& tVal_minus, const double& tVal, const double& tVal_plus, bool tNegFlux) {
-//    double V0 = 0.5 * tVal + 0.5 * tVal_plus;       //---r = 0---//
-//    double V1 = -0.5 * tVal_minus + 1.5 * tVal;     //---r = 1---//
-//
-//    double Beta0 = pow(tVal_plus - tVal, 2.0);
-//    double Beta1 = pow(tVal - tVal_minus, 2.0);
-//
-//    double Alpha0;
-//    double Alpha1;
-//
-//    if (tNegFlux) {
-//        Alpha0 = 1.0 / 3.0 / pow(1.0e-06 + Beta0, 2.0);
-//        Alpha1 = 2.0 / 3.0 / pow(1.0e-06 + Beta1, 2.0);
-//    } else {
-//        Alpha0 = 2.0 / 3.0 / pow(1.0e-06 + Beta0, 2.0);
-//        Alpha1 = 1.0 / 3.0 / pow(1.0e-06 + Beta1, 2.0);
-//    }
-//
-//    double Omega0 = Alpha0 / (Alpha0 + Alpha1);
-//    double Omega1 = Alpha1 / (Alpha0 + Alpha1);
-//
-//    return Omega0 * V0 + Omega1 * V1;
-//}
+//---Method---//
 
-dVector2D <double> WENO_2(const dVector2D <double>& tVal_minus, const dVector2D <double>& tVal, const dVector2D <double>& tVal_plus, bool tNegFlux) {
+dVector3D <double> WENO_2(const dVector3D <double>& tVal_minus, const dVector3D <double>& tVal, const dVector3D <double>& tVal_plus, bool tNegFlux) {
     auto V0 = 0.5 * tVal + 0.5 * tVal_plus;       //---r = 0---//
     auto V1 = -0.5 * tVal_minus + 1.5 * tVal;     //---r = 1---//
 
@@ -122,8 +73,8 @@ dVector2D <double> WENO_2(const dVector2D <double>& tVal_minus, const dVector2D 
         Beta1[i] *= Beta1[i];
     }
 
-    dVector2D <double> Alpha0;
-    dVector2D <double> Alpha1;
+    dVector3D <double> Alpha0;
+    dVector3D <double> Alpha1;
 
     if (tNegFlux) {
         for (int i = 0; i < Dim; i++) {
@@ -137,15 +88,15 @@ dVector2D <double> WENO_2(const dVector2D <double>& tVal_minus, const dVector2D 
         }
     }
 
-    dVector2D <double> Omega0;
-    dVector2D <double> Omega1;
+    dVector3D <double> Omega0;
+    dVector3D <double> Omega1;
 
     for (int i = 0; i < Dim; i++) {
         Omega0[i] = Alpha0[i] / (Alpha0[i] + Alpha1[i]);
         Omega1[i] = Alpha1[i] / (Alpha0[i] + Alpha1[i]);
     }
 
-    dVector2D <double> Res;
+    dVector3D <double> Res;
 
     for (int i = 0; i < Dim; i++) {
         Res[i] = Omega0[i] * V0[i] + Omega1[i] * V1[i];
@@ -154,91 +105,168 @@ dVector2D <double> WENO_2(const dVector2D <double>& tVal_minus, const dVector2D 
     return Res;
 }
 
+//---Method---//
+
 int main() {
     size_t Num = 600;
+    size_t Offset = 2;
     int TimeToCompute = 1;  //---Sec---//
 
-    std::vector <dVector2D <double>> GridCurrentData(Num);
-    std::vector <dVector2D <double>> GridTempData(Num);
+    std::vector <std::vector <dVector3D <double>>> GridCurrentData(Num + Offset * 2);
+    std::vector <std::vector <dVector3D <double>>> GridTempData(Num + Offset * 2);
 
-    std::vector <dVector2D <double>> *CurrentData = &GridCurrentData;
-    std::vector <dVector2D <double>> *TempData = &GridTempData;
+    std::vector <std::vector <dVector3D <double>>> *CurrentData = &GridCurrentData;
+    std::vector <std::vector <dVector3D <double>>> *TempData = &GridTempData;
 
-    std::vector <double> Bottom(Num);
+    std::vector <std::vector <double>> Bottom(Num + Offset * 2);
+    
+    for (size_t i = 0; i < Num + Offset * 2; i++) {
+        GridCurrentData[i].resize(Num + Offset * 2);
+        GridTempData[i].resize(Num + Offset * 2);
+
+        Bottom[i].resize(Num + Offset * 2);
+    }
+    
+    //----------//
+    
     bottomFunc(Bottom);
 
     std::ofstream EOutput;
     std::ofstream BottomOutput;
 
-    EOutput.open("EAmplitude.dat");
+    EOutput.open("Amplitude.dat");
     BottomOutput.open("BottomOutput.dat");
 
     initConditions((*CurrentData));
     initConditions((*TempData));
 
-    for (int i = 0; i < 1000; i++) {
-        double Alpha = 0.0;
+    for (int iTime = 0; iTime < 2000; iTime++) {
+        std::vector <double> AlphaX(Num + Offset * 2);
+        std::vector <double> AlphaY(Num + Offset * 2);
 
-        for (const auto& iNum : (*CurrentData)) {
-            Alpha = std::max(fabs(iNum[1] / iNum[0] + sqrt(9.81 * iNum[0])), Alpha);
-            Alpha = std::max(fabs(iNum[1] / iNum[0] - sqrt(9.81 * iNum[0])), Alpha);
-
-            if (iNum[0] <= 0) {
-                std::cout << iNum[0] << " " << i << std::endl;
+        for (size_t iY = Offset; iY < Num + Offset; iY++) {
+            for (size_t iX = Offset; iX < Num + Offset; iX++) {
+                AlphaX[iY] = std::max(fabs((*CurrentData)[iX][iY][1] / (*CurrentData)[iX][iY][0] + sqrt(9.81 * (*CurrentData)[iX][iY][0])), AlphaX[iY]);
+                AlphaX[iY] = std::max(fabs((*CurrentData)[iX][iY][1] / (*CurrentData)[iX][iY][0] - sqrt(9.81 * (*CurrentData)[iX][iY][0])), AlphaX[iY]);
+                AlphaX[iY] = std::max(fabs((*CurrentData)[iX][iY][1] / (*CurrentData)[iX][iY][0]), AlphaX[iY]);
             }
         }
 
-        for (size_t j = 2; j < Num - 2; j++) {
-            auto PlusPlus = WENO_2(
-                    (funcX((*CurrentData)[j - 1]) + Alpha * (*CurrentData)[j - 1]) / 2.0,
-                    (funcX((*CurrentData)[j]) + Alpha * (*CurrentData)[j]) / 2.0,
-                    (funcX((*CurrentData)[j + 1]) + Alpha * (*CurrentData)[j + 1]) / 2.0,
-                    false);
-            auto PlusMinus = WENO_2(
-                    (funcX((*CurrentData)[j]) - Alpha * (*CurrentData)[j]) / 2.0,
-                    (funcX((*CurrentData)[j + 1]) - Alpha * (*CurrentData)[j + 1]) / 2.0,
-                    (funcX((*CurrentData)[j + 2]) - Alpha * (*CurrentData)[j + 2]) / 2.0,
-                    true);
-
-            auto MinusPlus = WENO_2(
-                    (funcX((*CurrentData)[j - 2]) + Alpha * (*CurrentData)[j - 2]) / 2.0,
-                    (funcX((*CurrentData)[j - 1]) + Alpha * (*CurrentData)[j - 1]) / 2.0,
-                    (funcX((*CurrentData)[j]) + Alpha * (*CurrentData)[j]) / 2.0,
-                    false);
-            auto MinusMinus = WENO_2(
-                    (funcX((*CurrentData)[j - 1]) - Alpha * (*CurrentData)[j - 1]) / 2.0,
-                    (funcX((*CurrentData)[j]) - Alpha * (*CurrentData)[j]) / 2.0,
-                    (funcX((*CurrentData)[j + 1]) - Alpha * (*CurrentData)[j + 1]) / 2.0,
-                    true);
-
-            (*TempData)[j] =
-                    (*CurrentData)[j] -
-                    0.001 / 0.5 * (PlusPlus + PlusMinus - MinusPlus - MinusMinus) -
-                    0.001 * dVector2D <double>(0.0, 9.81 * (*CurrentData)[j][0] * (Bottom[j + 1] - Bottom[j - 1]) / 0.5);
+        for (size_t iX = Offset; iX < Num + Offset; iX++) {
+            for (size_t iY = Offset; iY < Num + Offset; iY++) {
+                AlphaY[iX] = std::max(fabs((*CurrentData)[iX][iY][2] / (*CurrentData)[iX][iY][0] + sqrt(9.81 * (*CurrentData)[iX][iY][0])), AlphaY[iX]);
+                AlphaY[iX] = std::max(fabs((*CurrentData)[iX][iY][2] / (*CurrentData)[iX][iY][0] - sqrt(9.81 * (*CurrentData)[iX][iY][0])), AlphaY[iX]);
+                AlphaX[iX] = std::max(fabs((*CurrentData)[iX][iY][2] / (*CurrentData)[iX][iY][0]), AlphaX[iX]);
+            }
         }
 
-        (*TempData)[1][0] = 100.0;
-        (*TempData)[0][0] = 100.0;
-        (*TempData)[Num - 2][0] = 2.0 * (*TempData)[Num - 3][0] - (*TempData)[Num - 4][0];
-        (*TempData)[Num - 1][0] = (*TempData)[Num - 2][0];
+        std::cout << iTime << ": " << (*CurrentData)[302][302] << std::endl;
 
-        (*TempData)[1][1] = 2.0 * (*TempData)[2][1] - (*TempData)[3][1];
-        (*TempData)[0][1] = (*TempData)[1][1];
-        (*TempData)[Num - 2][1] = 2.0 * (*TempData)[Num - 3][0] - (*TempData)[Num - 4][0];
-        (*TempData)[Num - 1][1] = (*TempData)[Num - 2][0];
+        for (size_t i = Offset; i < Num + Offset; i++) {
+            for (size_t j = Offset; j < Num + Offset; j++) {
+                auto PlusPlusX = WENO_2(
+                        (funcX((*CurrentData)[i - 1][j]) + AlphaX[j] * (*CurrentData)[i - 1][j]) / 2.0,
+                        (funcX((*CurrentData)[i][j]) + AlphaX[j] * (*CurrentData)[i][j]) / 2.0,
+                        (funcX((*CurrentData)[i + 1][j]) + AlphaX[j] * (*CurrentData)[i + 1][j]) / 2.0,
+                        false);
+                auto PlusMinusX = WENO_2(
+                        (funcX((*CurrentData)[i][j]) - AlphaX[j] * (*CurrentData)[i][j]) / 2.0,
+                        (funcX((*CurrentData)[i + 1][j]) - AlphaX[j] * (*CurrentData)[i + 1][j]) / 2.0,
+                        (funcX((*CurrentData)[i + 2][j]) - AlphaX[j] * (*CurrentData)[i + 2][j]) / 2.0,
+                        true);
+
+                auto MinusPlusX = WENO_2(
+                        (funcX((*CurrentData)[i - 2][j]) + AlphaX[j] * (*CurrentData)[i - 2][j]) / 2.0,
+                        (funcX((*CurrentData)[i - 1][j]) + AlphaX[j] * (*CurrentData)[i - 1][j]) / 2.0,
+                        (funcX((*CurrentData)[i][j]) + AlphaX[j] * (*CurrentData)[i][j]) / 2.0,
+                        false);
+                auto MinusMinusX = WENO_2(
+                        (funcX((*CurrentData)[i - 1][j]) - AlphaX[j] * (*CurrentData)[i - 1][j]) / 2.0,
+                        (funcX((*CurrentData)[i][j]) - AlphaX[j] * (*CurrentData)[i][j]) / 2.0,
+                        (funcX((*CurrentData)[i + 1][j]) - AlphaX[j] * (*CurrentData)[i + 1][j]) / 2.0,
+                        true);
+                
+                //----------//
+                
+                auto PlusPlusY = WENO_2(
+                        (funcY((*CurrentData)[i][j - 1]) + AlphaY[i] * (*CurrentData)[i][j - 1]) / 2.0,
+                        (funcY((*CurrentData)[i][j]) + AlphaY[i] * (*CurrentData)[i][j]) / 2.0,
+                        (funcY((*CurrentData)[i][j + 1]) + AlphaY[i] * (*CurrentData)[i][j + 1]) / 2.0,
+                        false);
+                auto PlusMinusY = WENO_2(
+                        (funcY((*CurrentData)[i][j]) - AlphaY[i] * (*CurrentData)[i][j]) / 2.0,
+                        (funcY((*CurrentData)[i][j + 1]) - AlphaY[i] * (*CurrentData)[i][j + 1]) / 2.0,
+                        (funcY((*CurrentData)[i][j + 2]) - AlphaY[i] * (*CurrentData)[i][j + 2]) / 2.0,
+                        true);
+
+                auto MinusPlusY = WENO_2(
+                        (funcY((*CurrentData)[i][j - 2]) + AlphaY[i] * (*CurrentData)[i][j - 2]) / 2.0,
+                        (funcY((*CurrentData)[i][j - 1]) + AlphaY[i] * (*CurrentData)[i][j - 1]) / 2.0,
+                        (funcY((*CurrentData)[i][j]) + AlphaY[i] * (*CurrentData)[i][j]) / 2.0,
+                        false);
+                auto MinusMinusY = WENO_2(
+                        (funcY((*CurrentData)[i][j - 1]) - AlphaY[i] * (*CurrentData)[i][j - 1]) / 2.0,
+                        (funcY((*CurrentData)[i][j]) - AlphaY[i] * (*CurrentData)[i][j]) / 2.0,
+                        (funcY((*CurrentData)[i][j + 1]) - AlphaY[i] * (*CurrentData)[i][j + 1]) / 2.0,
+                        true);
+
+                (*TempData)[i][j] =
+                        (*CurrentData)[i][j] -
+                        0.001 / 0.5 * (PlusPlusX + PlusMinusX - MinusPlusX - MinusMinusX) -
+                        0.001 / 0.5 * (PlusPlusY + PlusMinusY - MinusPlusY - MinusMinusY) -
+                        0.001 *
+                        dVector3D <double> (
+                                0.0,
+                                9.81 * (*CurrentData)[i][j][0] * (Bottom[i + 1][j] - Bottom[i - 1][j]),
+                                9.81 * (*CurrentData)[i][j][0] * (Bottom[i][j + 1] - Bottom[i][j - 1]));
+            }
+        }
+
+        for (size_t i = 1; i < Num + 2; i++) {
+            (*TempData)[i][1][0] = 100.0;
+            (*TempData)[i][0][0] = 100.0;
+            (*TempData)[i][Num + 2][0] = 2.0 * (*TempData)[i][Num + 1][0] - (*TempData)[i][Num][0];
+            (*TempData)[i][Num + 3][0] = (*TempData)[i][Num + 2][0];
+
+            (*TempData)[i][1][1] = 2.0 * (*TempData)[i][2][1] - (*TempData)[i][3][1];
+            (*TempData)[i][0][1] = (*TempData)[i][1][1];
+            (*TempData)[i][Num + 2][1] = 2.0 * (*TempData)[i][Num + 1][1] - (*TempData)[i][Num][1];
+            (*TempData)[i][Num + 3][1] = (*TempData)[i][Num + 2][1];
+
+            (*TempData)[i][1][2] = 2.0 * (*TempData)[i][2][2] - (*TempData)[i][3][2];
+            (*TempData)[i][0][2] = (*TempData)[i][1][2];
+            (*TempData)[i][Num + 2][2] = 2.0 * (*TempData)[i][Num + 1][2] - (*TempData)[i][Num][2];
+            (*TempData)[i][Num + 3][2] = (*TempData)[i][Num + 2][2];
+        }
+
+        for (size_t j = 0; j < Num + 2 * Offset; j++) {
+            for (int k = 0; k < 3; k++) {
+                (*TempData)[1][j][k] = (*TempData)[2][j][k];
+                (*TempData)[0][j][k] = (*TempData)[3][j][k];
+
+                (*TempData)[Num + 2][j][k] = (*TempData)[Num + 1][j][k];
+                (*TempData)[Num + 3][j][k] = (*TempData)[Num][j][k];
+            }
+        }
 
         std::swap(CurrentData, TempData);
     }
 
     //-----------------------------//
 
-    for (size_t i = 0; i < Num; i++) {
-        EOutput << (*CurrentData)[i][0] << " ";
+    for (size_t i = Offset; i < Num + Offset; i++) {
+        for (size_t j = Offset; j < Num + Offset; j++) {
+            EOutput << (*CurrentData)[i][j][0] << " ";
+        }
+
+        EOutput << std::endl;
     }
 
-    for (size_t i = 0; i < Num; i++) {
-        BottomOutput << Bottom[i] << " ";
-    }
+    EOutput.close();
+//
+//    for (size_t i = 0; i < Num; i++) {
+//        BottomOutput << Bottom[i] << " ";
+//    }
 
     return 0;
 }
