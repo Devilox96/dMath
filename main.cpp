3783a769a282fd9d47059aceb839438344916508
Oleg Skinderev <skinderev@gmail.com>
2021-02-10 15:24:54 +0300

    remove unused code.

--- main.cpp
+++ main.cpp
@@ -14,25 +14,17 @@ void test(F func, Args&&... args){
     }
     std::clock_t end = std::clock();
 
-    std::cout <<  double(end - start) / CLOCKS_PER_SEC << " s\n";
+    std::cout <<  double(end - start) << " ns\n";
 }
 
 
 int main() {
+    test([](){
+        dVector4D<double> d1(5);
+        dVector4D<double> d2(5);
+        auto d = d1 * d2;
+    });
 
-//    dVector4D <double> d1;
-    dVector <double, 9> d2;
-    dVector <double, 9> d3;
-
-    d2.fill(10);
-    d3.fill(-3);
-    //    test(d2.null);
-//    std::cout << d2.norm_2() << std::endl;
-//    std::cout << d2.norm() << std::endl;
-//    test(std::bind(&dVector <double, 9>::operator+=, d2, d3));
-//    test([&d2, &d3](){d2+=d3;});
-    test([&d2](){d2.norm();});
-    test([&d2](){d2.norm_2();});
 
     return EXIT_SUCCESS;
 }
