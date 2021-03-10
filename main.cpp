a992171fd5b542b9d941d6c19beb7508bf4a7535
Oleg Skinderev <skinderev@gmail.com>
2021-03-10 22:59:32 +0300

    add example for iterators.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <utility>
 #include "Core/dVector.h"
+#include "Core/dMatrix.h"
 
 #include <functional>
 #include <ctime>
@@ -19,11 +20,28 @@ void test(F func, Args&&... args){
 
 
 int main() {
-    test([](){
-        dVector4D<double> d1(5);
-        dVector4D<double> d2(5);
-        auto d = d1 * d2;
-    });
+    dSMatrix<double, 3> m({1, 2, 3, 4, 5, 6, 7, 8, 9});
+    for(auto row = m.begin<Iterate::Row>(); row != m.end<Iterate::Row>(); ++row) {
+        for(auto i = row->begin(); i != row->end(); ++i) {
+            std::cout << *i << "(" << std::distance(m.begin(), i) << ") ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << *m.begin<Iterate::Col>()->begin() << std::endl;
+
+    for(auto col = m.begin<Iterate::Col>(); col != m.end<Iterate::Col>(); ++col) {
+        for(auto i = col->begin(); i != col->end(); ++i) {
+            std::cout << *i << "(" << std::distance(m.begin(), static_cast<double*>(i)) << ") ";
+        }
+        std::cout << std::endl;
+    }
+
+//    test([](){
+//        dVector4D<double> d1(5);
+//        dVector4D<double> d2(5);
+//        auto d = d1 * d2;
+//    });
 
 
     return EXIT_SUCCESS;
