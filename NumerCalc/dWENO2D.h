4f0d6b7b6cc66eae1f03019275291e75a45236db
Andrey <devilox96@yandex.ru>
2021-04-18 01:32:59 +0300

    Added solve function; need to finish plotting to test new changes

--- NumerCalc/dWENO2D.h
+++ NumerCalc/dWENO2D.h
@@ -13,6 +13,91 @@
  */
 template <class T>
 class dWENO2D {
+public:
+    void setTimeStep(double tStep) {
+        mStepTime = tStep;
+
+        if (mStepX != 0) {
+            mRatioX = mStepTime / mStepX;
+        }
+
+        if (mStepY != 0) {
+            mRatioY = mStepTime / mStepY;
+        }
+    }
+    void setXStep(double tStep) {
+        mStepX = tStep;
+
+        if (mStepX != 0) {
+            mRatioX = mStepTime / mStepX;
+        }
+    }
+    void setYStep(double tStep) {
+        mStepY = tStep;
+
+        if (mStepY != 0) {
+            mRatioY = mStepTime / mStepY;
+        }
+    }
+
+    //----------//
+
+    /**
+     *
+     * @param tOffsetZero Grid[i][j]
+     * @param tX_min_1_Y Grid[i - 1][j]
+     * @param tX_min_2_Y Grid[i - 2][j]
+     * @param tX_pl_1_Y Grid[i + 1][j]
+     * @param tX_pl_2_Y Grid[i + 2][j]
+     * @param tX_Y_min_1 Grid[i][j - 1]
+     * @param tX_Y_min_2 Grid[i][j - 2]
+     * @param tX_Y_pl_1 Grid[i][j + 1]
+     * @param tX_Y_pl_2 Grid[i][j + 2]
+     * @param tMaxAlpha Maximum module of all eigen values of the matrix,
+     * used for Lax-Friedrichs flux splitting
+     */
+    T solve(const T& tOffsetZero,
+               const T& tX_min_1_Y,         const T& tX_min_2_Y,
+               const T& tX_pl_1_Y,          const T& tX_pl_2_Y,
+               const T& tX_Y_min_1,         const T& tX_Y_min_2,
+               const T& tX_Y_pl_1,          const T& tX_Y_pl_2,
+               const T& tMaxAlpha) {
+        auto PlusX = approximate(
+                (funcX(tX_min_1_Y) + tMaxAlpha * tX_min_1_Y) / 2.0,
+                (funcX(tOffsetZero) + tMaxAlpha * tOffsetZero) / 2.0,
+                (funcX(tX_pl_1_Y) + tMaxAlpha * tX_pl_1_Y) / 2.0,
+                (funcX(tOffsetZero) - tMaxAlpha * tOffsetZero) / 2.0,
+                (funcX(tX_pl_1_Y) - tMaxAlpha * tX_pl_1_Y) / 2.0,
+                (funcX(tX_pl_2_Y) - tMaxAlpha * tX_pl_2_Y) / 2.0
+        );
+        auto MinusX = approximate(
+                (funcX(tX_min_2_Y) + tMaxAlpha * tX_min_2_Y) / 2.0,
+                (funcX(tX_min_1_Y) + tMaxAlpha * tX_min_1_Y) / 2.0,
+                (funcX(tOffsetZero) + tMaxAlpha * tOffsetZero) / 2.0,
+                (funcX(tX_min_1_Y) - tMaxAlpha * tX_min_1_Y) / 2.0,
+                (funcX(tOffsetZero) - tMaxAlpha * tOffsetZero) / 2.0,
+                (funcX(tX_pl_1_Y) - tMaxAlpha * tX_pl_1_Y) / 2.0
+        );
+
+        auto PlusY = approximate(
+                (funcY(tX_Y_min_1) + tMaxAlpha * tX_Y_min_1) / 2.0,
+                (funcY(tOffsetZero) + tMaxAlpha * tOffsetZero) / 2.0,
+                (funcY(tX_Y_pl_1) + tMaxAlpha * tX_Y_pl_1) / 2.0,
+                (funcY(tOffsetZero) - tMaxAlpha * tOffsetZero) / 2.0,
+                (funcY(tX_Y_pl_1) - tMaxAlpha * tX_Y_pl_1) / 2.0,
+                (funcY(tX_Y_pl_2) - tMaxAlpha * tX_Y_pl_2) / 2.0
+        );
+        auto MinusY = approximate(
+                (funcY(tX_Y_min_2) + tMaxAlpha * tX_Y_min_2) / 2.0,
+                (funcY(tX_Y_min_1) + tMaxAlpha * tX_Y_min_1) / 2.0,
+                (funcY(tOffsetZero) + tMaxAlpha * tOffsetZero) / 2.0,
+                (funcY(tX_Y_min_1) - tMaxAlpha * tX_Y_min_1) / 2.0,
+                (funcY(tOffsetZero) - tMaxAlpha * tOffsetZero) / 2.0,
+                (funcY(tX_Y_pl_1) - tMaxAlpha * tX_Y_pl_1) / 2.0
+        );
+
+        return tOffsetZero - ((PlusX - MinusX) * mRatioX + (PlusY - MinusY) * mRatioY);
+    }
 protected:
     virtual T funcX(const T& tVal) {
         return tVal;
@@ -21,7 +106,16 @@ protected:
         return tVal;
     }
 private:
-    ///---TODO: substitute pow for plain multiplication in case of single precision---//
+    double mStepTime = 0.0;
+
+    double mStepX = 0.0;
+    double mStepY = 0.0;
+
+    double mRatioX = 0.0;           //---mStepTime / mStepX---//
+    double mRatioY = 0.0;           //---mStepTime / mStepY---//
+
+    //----------//
+    
     T approximate(
             const T& tPosVal_minus_1,
             const T& tPosVal,
